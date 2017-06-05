ecc3e5ec66837a240a69e833219b501b8dca26a3
Tazovsky <tazzpl@gmail.com>
2017-06-05 21:08:34 +0200

    list2char ok

--- R/01-list2char.R
+++ R/01-list2char.R
@@ -1,9 +1,9 @@
 #' @title list2char
-#' @description Funkcja zamieniająca klasę list na string w kolumnach data.table
+#' @description Changes 'list' class to 'character' in data.table column
 #' @import data.table
-#' @param data data.table; dane w postaci data.table 
-#' @param except character vector; nazwy kolumn podawane w wektorze, których nie chcemy zamieniać na character
-#' @param sep character; separator zamienianych kolumn w data.table; jeśli NULL to domyślnie jest to ", "
+#' @param data data.table; 
+#' @param except character vector; column names we don't want to change to character
+#' @param sep character; to separate values; if NULL then value is set to ", "; 
 #'
 #' @return data.table 
 #' @export
@@ -12,23 +12,23 @@
 #' data_no_list <- list2char(data)
 list2char <- function(data, except = NULL, sep = NULL){
   
+  stopifnot(is.data.table(data))
+  
   if (any(sapply(data, is.list)) == F ) {
-    message("W danych nie ma zadnej kolumny klasy 'list'. Zwracam niezmieniony argument 'data'.")
+    warning("None of columns is 'list' class. Returns unchanged 'data'.")
     return(data)
   }
   
-  stopifnot(is.data.table(data))
-  
-  if ( length(except) > 0 && is.vector(except) == F )
-    stop("Argument 'except' nie jest wektorem.")
+  if ( is.character(except) == F & length(except) > 0  )
+    stop("'except' is not a character vector.")
   
   cols_not_in <- except[!except %in% names(data)]
   
   if (length(cols_not_in) > 0 )
-    stop(sprintf("Niepoprawne nazwy kolumn w argumencie 'except': %s.", paste0(cols_not_in, collapse = ", ")))
+    stop(sprintf("There are no columns like: %s.", paste0(cols_not_in, collapse = ", ")))
   
   if (!is.null(sep) && !is.character(sep))
-    stop("Argument 'sep' musi byc klasy 'character'")
+    stop("'sep' is not a character class")
   
   # nie chcemy zeby zmienila sie struktura danych wejsciowych poprzez referencje do pamieci stad robimy jej kopie w pamieci
   data <- copy(data)
--- tests/testthat/test_list2char.R
+++ tests/testthat/test_list2char.R
@@ -1,3 +1,5 @@
+context("list2char")
+
 test_that("list2char works with of list columns in data.table", {
   d1 <- data.table(a = c(1,2,3), b = list(c("a", "b"), "c", c("d", "e")))
   d2 <- data.table(a = list(c(1,2,3), c(4, 5, 6), c(7, 8)), b = list(c("a", "b"), "c", c("d", "e")))
@@ -11,3 +13,28 @@ test_that("list2char works with of list columns in data.table", {
   expect_equal(list2char(d3, sep = ","), d3)
   
 })
+
+test_that("list2char works with data.table without any list columns", {
+  expect_warning(list2char(data.table(mtcars)), "None of columns is 'list' class")
+})
+
+test_that("'data' argument is data.table", {
+  expect_error(list2char(mtcars), "is\\.data\\.table.*TRUE")
+})
+
+test_that("'expect' is vector", {
+  d2 <- data.table(a = list(c(1,2,3), c(4, 5, 6), c(7, 8)), b = list(c("a", "b"), "c", c("d", "e")))
+  expect_error(list2char(d2, except = list("a", "b") ), "not a character vector")
+})
+
+test_that("'expect' cointains correct column names", {
+  d2 <- data.table(a = list(c(1,2,3), c(4, 5, 6), c(7, 8)), b = list(c("a", "b"), "c", c("d", "e")))
+  expect_error(list2char(d2, except = c("d", "b") ), "no columns")
+})
+
+test_that("separator has to be a character class", {
+  d2 <- data.table(a = list(c(1,2,3), c(4, 5, 6), c(7, 8)), b = list(c("a", "b"), "c", c("d", "e")))
+  expect_error(list2char(d2, sep = 1 ), "not a character")
+})
+
+
